a5f9479d6a4ef9f52c6ab6e7a263ebb5ddd0378f
schairez <34615630+schairez@users.noreply.github.com>
2020-06-29 00:22:11 -0700

    data_hist_fetch_done

--- src/app_historic_tweets.py
+++ src/app_historic_tweets.py
@@ -33,14 +33,14 @@ def prep_tweet_data(arr_tweet_objs: List) -> List[Dict[str, str]]:
 
 
 def main():
-    date_ranges_list = list(get_date_ranges_gen('2020-01-01', '2020-06-28'))
+    date_ranges_list = list(get_date_ranges_gen('2020-06-17', '2020-06-28'))
     for date_tuple in date_ranges_list:
         month_idx = int(date_tuple[0][5:7])
         print(month_idx)
         month_abbr_text = calendar.month_abbr[month_idx]
         tweet_data = get_tweets("DACA", *date_tuple)
         log.info(
-            f"Got list of tweed data for date:{tweet_data[0]} with len: {len(tweet_data)}")
+            f"Got list of tweed data for date:{date_tuple[0]} with len: {len(tweet_data)}")
         tweet_data_clean = prep_tweet_data(tweet_data)
         log.info(f"tweet data objects are preprocessed")
         # write_JSON_file(
