e000d17b28826d3b44b2e96cd6158a6869599c07
schairez <34615630+schairez@users.noreply.github.com>
2020-06-28 19:33:57 -0700

    updated_flat_file_push

---
+++ file_tools/json_tools.py
@@ -0,0 +1,27 @@
+import json
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s,%(msecs)d %(levelname)s: %(message)s",
+    datefmt="%H:%M:%S",
+)
+log = logging
+
+
+def write_JSON_file(output_file_name_w_path, arr_of_objs, json_lines=False):
+    with open(output_file_name_w_path, mode="ab", encoding='utf8') as out_file:
+        if json_lines:
+            for d in arr_of_objs:
+                json.dump(d, out_file, ensure_ascii=False)
+                out_file.write('\n')
+        else:
+            json.dump(arr_of_objs, out_file, indent=2,
+                      sort_keys=False, ensure_ascii=False)
+        log.info("Wrote results for file_name: %s", output_file_name_w_path)
+
+
+def read_JSON_file(input_file_name):
+    with open(input_file_name) as json_file:
+        return json.load(json_file)
