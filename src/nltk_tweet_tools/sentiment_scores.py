eba06ff8b8cdb1ca48d9666f0af24263b13c8217
schairez <34615630+schairez@users.noreply.github.com>
2020-06-30 23:46:05 -0700

    updated_qual_sent_scores

--- src/nltk_tweet_tools/sentiment_scores.py
+++ src/nltk_tweet_tools/sentiment_scores.py
@@ -8,10 +8,15 @@ def sentiment_analyzer_scores(sentence: str) -> Tuple[str, float]:
     sentiment_dict = analyser.polarity_scores(sentence)
 
     compound_sent_score: float = sentiment_dict['compound']
-    if compound_sent_score >= 0.05:
+    if 0.5 < compound_sent_score <= 1:
+        return ("highly positive", compound_sent_score)
+    elif 0.05 <= compound_sent_score <= 0.5:
         return ("positive", compound_sent_score)
 
-    elif compound_sent_score <= - 0.05:
+    elif -0.5 > compound_sent_score >= -1:
+        return ("highly negative", compound_sent_score)
+
+    elif -0.05 >= compound_sent_score >= -0.5:
         return ("negative", compound_sent_score)
 
     else:
@@ -20,3 +25,12 @@ def sentiment_analyzer_scores(sentence: str) -> Tuple[str, float]:
 
 # TIME FORMAT
 # 2020-06-19T00:12:45.511Z
+
+#    if compound_sent_score >= 0.05:
+#         return ("positive", compound_sent_score)
+
+#     elif compound_sent_score <= - 0.05:
+#         return ("negative", compound_sent_score)
+
+#     else:
+#         return ("neutral", compound_sent_score)
